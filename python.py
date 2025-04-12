f229f9d307b8e62e9dc4c2cf4cbcaee6ef173677
Harshit <harshit96310@gmail.com>
2025-04-12 12:01:16 +0530

    second commit in the file

--- python.py
+++ python.py
@@ -1,3 +1,5 @@
 #just practicing git and github
 
 print("Hii this a python file created by me for practicing github file ")
+#it will now print somethin else 
+print("it is the second commit on the github practice ")
